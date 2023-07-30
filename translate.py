085f0d705a7a74933a9078cea6cb45ad29c71e94
Gabrijel Boduljak <gboduljak@outlook.com>
2023-07-30 07:32:17 +0200

    minor

--- translate.py
+++ translate.py
@@ -91,8 +91,9 @@ if __name__ == '__main__':
 
   if opt.eval:
     model.eval()
-
-  model_translations_dir = Path('translations')
+  if not os.path.exists('translations'):
+    os.mkdir('translations')
+  model_translations_dir = Path('translations', opt.name)
   if not os.path.exists(model_translations_dir):
     os.mkdir(model_translations_dir)
   model_with_iters_translations_dir = Path(model_translations_dir, f'{ckpt}')
